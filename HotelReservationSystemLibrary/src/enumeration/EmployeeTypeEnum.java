6e36a6c80242ee347973826b951efbf65eb949e6
Jett-Tan <68832631+Jett-Tan@users.noreply.github.com>
2024-10-22 01:22:05 +0800

    EnumCreated
    EmployeeTypeEnum

---
+++ HotelReservationSystemLibrary/src/enumeration/EmployeeTypeEnum.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package enumeration;
+
+/**
+ *
+ * @author Tan Jian Feng
+ */
+public enum EmployeeTypeEnum {
+    EMPLOYEE,
+    SALES,
+    SYSTEMADMINISTRATOR,
+    OPERATIONMANAGER,
+    GUESTRELATIONOFFICER
+}
